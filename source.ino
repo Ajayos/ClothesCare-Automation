3427f4b3f433be77a23d801b15e6c242aa9b8bdc
Ajay o s <ajayosakhub@gmail.com>
2023-06-20 00:14:22 +0530

    updated the last test code

--- source.ino
+++ source.ino
@@ -7,8 +7,8 @@ const int forwardButtonPin = 6;   // Pin connected to the forward button
 const int backwardButtonPin = 7;  // Pin connected to the backward button
 
 // Define the pin connections for the stepper motor driver
-const int stepPin = 2;
-const int dirPin = 3;
+const int stepPin = 3;
+const int dirPin = 2;
 const int sleepPin = 4;  // Sleep control pin
 const int resetPin = 5;  // Reset control pin
 
@@ -16,12 +16,13 @@ const int mk = 8;
 
 const int rainThreshold = 700;     // Threshold value for rain detection
 
-const int stepsPerRevolution = 200; // Define the steps per revolution for the stepper motor
+const int stepsPerRevolution = 250; // Define the steps per revolution for the stepper motor
 
 const int stepDelay = 2000; // Define the delay between steps (in microseconds)
 
 boolean motorOn = false; // Motor status
 boolean isLight = false; // Light status
+boolean isRain = false; // Rain status
 boolean saveMovement = false; // Save movement for automatic backward operation
 boolean forwardButtonPressed = false; // Forward button state
 boolean backwardButtonPressed = false; // Backward button state
@@ -48,39 +49,41 @@ void loop() {
 
   if (lightLevel > lightThreshold) {
     isLight = true;
-    // digitalWrite(rainLightPin, HIGH); // Turn on the "Rain" indicator LED
   } else {
     isLight = false;
-    // digitalWrite(rainLightPin, LOW); // Turn off the "Rain" indicator LED
   }
 
   forwardButtonPressed = digitalRead(forwardButtonPin) == LOW;
   backwardButtonPressed = digitalRead(backwardButtonPin) == LOW;
 
   if (forwardButtonPressed) {
-    digitalWrite(mk, LOW); // Turn on the "On" indicator LED
-    
     forward();  // Run motor forward
+    motorOn = true;
     saveMovement = true;
   }
   if (backwardButtonPressed) {
-    
-    digitalWrite(mk, HIGH); // Turn on the "On" indicator LED
     backward(); // Run motor backward
+    motorOn = false;
     saveMovement = false;
   }
   if (rainSensorValue < rainThreshold) {
-    if (isLight && !motorOn) {
-      
+    isRain = true;
+  } else {
+    isRain = false;
+  }
+  if(isLight && !isRain && !motorOn) {
       forward();  // Run motor forward
       motorOn = true;
       saveMovement = true;
-    }
-  } else {
-    if (isLight && motorOn && !saveMovement) {
-      backward();  // Run motor backward
-      motorOn = false;
-    }
+  }
+
+  if(!isLight && motorOn) {
+    backward();  // Run motor backward
+    motorOn = false;
+  }
+  if(isRain && motorOn) {
+    backward();  // Run motor backward
+    motorOn = false;
   }
 
   delay(500);  // Delay for stability
@@ -110,7 +113,7 @@ void forward() {
 
   digitalWrite(dirPin, HIGH);  // Set the motor direction to forward
 
-  for (int i = 0; i < (10 * stepsPerRevolution); i++) {
+  for (int i = 0; i < (15 * stepsPerRevolution); i++) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(stepDelay);
     digitalWrite(stepPin, LOW);
@@ -119,9 +122,10 @@ void forward() {
 
   digitalWrite(sleepPin, LOW);  // Put the motor to sleep
   digitalWrite(resetPin, LOW);
+  
 
   // digitalWrite(forwardLightPin, LOW); // Turn off the "Forward" indicator LED
-}
+}  
 
 void backward() {
   // digitalWrite(backwardLightPin, HIGH); // Turn on the "Backward" indicator LED
@@ -131,7 +135,7 @@ void backward() {
 
   digitalWrite(dirPin, LOW);  // Set the motor direction to backward
 
-  for (int i = 0; i < (10 * stepsPerRevolution); i++) {
+  for (int i = 0; i < (15 * stepsPerRevolution); i++) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(stepDelay);
     digitalWrite(stepPin, LOW);
